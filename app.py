15b8229c34aebb9dbf46339a716bacf95d549091
Luis Marenco <luismanuel01277lmmt@gmail.com>
2024-03-08 15:00:45 -0400

    5to commit

--- app.py
+++ app.py
@@ -65,6 +65,3 @@ def crear_registro():
 if __name__ == '__main__':
     app.secret_key="Luis2863"
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
-
-#hola skdksd skksks
-print("hola como estas")
